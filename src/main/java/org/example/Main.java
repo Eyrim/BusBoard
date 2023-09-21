50fbe0cf810ef813504d2c8fcb700bcc4a664d1f
Eyrim <marfxedbot@gmail.com>
2023-09-21 16:44:33 +0100

    Done some refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,16 +14,13 @@ import java.util.*;
 
 public class Main {
     private static final String BASE_TFL_URL = "https://api.tfl.gov.uk/StopPoint/%s/Arrivals";
-    private static final String BASE_POSTCODE_URL = "https://api.postcodes.io/postcodes/%s";
-                                                                                                            //  Dice rolling this as the right type
-    private static final String BASE_STOPPOINT_URL = "https://api.tfl.gov.uk/StopPoint/?lat=%s&lon=%s&stopTypes=NaptanOnstreetBusCoachStopPair&radius=500";
 
     public static void main(String[] args) {
         try { // 490000129R
             BusData[] data;
             //URL tflUrl = new URL(String.format(BASE_TFL_URL, getValueFromUser("Enter StopPoint ID:")));
             URL tflUrl;
-            StopPointData.StopPoint[] stopPoints = getStopPoints();
+            StopPointData.StopPoint[] stopPoints = StopPointData.getStopPoints(getValueFromUser("Enter Postcode:"));
 
             for (StopPointData.StopPoint s : stopPoints) {
                 tflUrl = new URL(String.format(BASE_TFL_URL, s.getLines()));
@@ -50,89 +47,11 @@ public class Main {
         }
     }
 
-    private static PostcodeResult getPostCodeData() {
-        try {
-            URL postcodeUrl = new URL(String.format(BASE_POSTCODE_URL, getValueFromUser("Enter Postcode:")));
-            HttpURLConnection con = (HttpURLConnection) postcodeUrl.openConnection();
-            Gson gson = new GsonBuilder().create();
-            PostcodeResult postcodeData;
 
-            if (con.getResponseCode() == 200) {
-                postcodeData = readPostcodeData(con.getInputStream());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    private static StopPointData.StopPoint[] getStopPoints() {
-        try {
-            URL stopPointUrl;
-            PostcodeResult postCodeData = getPostCodeData();
-            HttpURLConnection con;
-            Gson gson = new GsonBuilder().create();
-            StopPointData stopPointData;
 
-            stopPointUrl = new URL(String.format(
-                    BASE_STOPPOINT_URL,
-                    postCodeData.getData().getLatitude(),
-                    postCodeData.getData().getLongitude())
-            );
-            con = (HttpURLConnection) stopPointUrl.openConnection();
-            con.setRequestMethod("GET");
 
-            stopPointData = readStopPointData(con.getInputStream());
 
-            if (stopPointData.getStopPoints().length == 0) {
-                throw new RuntimeException("NO STOP POINTS IN THAT POSTCODE");
-            }
-
-            return stopPointData.getStopPoints();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static StopPointData readStopPointData(InputStream in) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            StopPointData stopPointData;
-            Gson gson = new GsonBuilder().create();
-            String inputLine;
-            StringBuilder content = new StringBuilder();
-
-            while ((inputLine = br.readLine()) != null) {
-                content.append(inputLine);
-            }
-
-            stopPointData = gson.fromJson(content.toString(), StopPointData.class);
-
-            return stopPointData;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static PostcodeResult readPostcodeData(InputStream in) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            PostcodeResult postcodeData;
-            Gson gson = new GsonBuilder().create();
-            String inputLine;
-            StringBuilder content = new StringBuilder();
-
-            while ((inputLine = br.readLine()) != null) {
-                content.append(inputLine);
-            }
-
-            postcodeData = gson.fromJson(content.toString(), PostcodeResult.class);
-
-            return postcodeData;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     private static BusData[] readBusData(InputStream in) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
--- src/main/java/org/example/PostcodeResult.java
+++ src/main/java/org/example/PostcodeResult.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class PostcodeResult {
     @SerializedName("result")
@@ -10,7 +19,7 @@ public class PostcodeResult {
         return data;
     }
 
-    public class PostcodeData {
+    public static class PostcodeData {
         @SerializedName("postcode")
         private String postcode;
         @SerializedName("latitude")
@@ -31,4 +40,40 @@ public class PostcodeResult {
         }
     }
 
+    private static final String BASE_POSTCODE_URL = "https://api.postcodes.io/postcodes/%s";
+
+    public static PostcodeResult getPostCodeData(String postCode) {
+        try {
+            URL postcodeUrl = new URL(String.format(BASE_POSTCODE_URL, postCode));
+            HttpURLConnection con = (HttpURLConnection) postcodeUrl.openConnection();
+            Gson gson = new GsonBuilder().create();
+
+            if (con.getResponseCode() == 200) {
+                return readPostcodeData(con.getInputStream());
+            } else {
+                throw new RuntimeException("BAD RESPONSE CODE");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static PostcodeResult readPostcodeData(InputStream in) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            PostcodeResult postcodeData;
+            Gson gson = new GsonBuilder().create();
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+
+            while ((inputLine = br.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            postcodeData = gson.fromJson(content.toString(), PostcodeResult.class);
+
+            return postcodeData;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
